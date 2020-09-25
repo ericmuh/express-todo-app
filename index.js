4a8746264e4e07a8e93d44fec8609e40fccaae07
ericmuh <muhwezi1000@gmail.com>
2020-09-25 12:39:21 +0300

    add mongodb driver

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const ehbs = require("express-handlebars");
+const MongoClient = require("mongodb").MongoClient;
+const assert = require("assert");
+
+// Connection URL
+const url = "mongodb://localhost:27017";
+
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 3000;
+// connecting to mongodb using mongodbClient driver
 
 //* Body parser middleware
 app.use(express.json());
@@ -18,7 +25,21 @@ app.get("/", (req, res) => {
 });
 // route rendering out out template todo.hbs //*GET TODOS: READ
 app.get("/todos", (req, res) => {
-  res.render("todo", { todos: todostore });
+  MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+    const db = client.db("todoapp");
+    const todos = db
+      .collection("todos")
+      .find()
+      .toArray((err, data) => {
+        assert.equal(null, err);
+        console.log(data);
+        res.render("todo", { todos: data });
+
+        client.close();
+      });
+  });
 });
 
 //! CRUD -create Read Update and Delete
@@ -36,11 +57,7 @@ app.post("/todos", (req, res) => {
 });
 
 // *API ENDPOINT
-app.use("/api/todos", require("./routes/api/todos"))
-
-
-
-
+app.use("/api/todos", require("./routes/api/todos"));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
--- routes/api/todos.js
+++ routes/api/todos.js
@@ -9,16 +9,15 @@ router.get("/", (req, res) => {
 
 // *GET A SINGLE TODO
 router.get("/:id", (req, res) => {
-    console.log(req.params.id);
-    const singleTodo= todos.filter((todo)=>{
-        return todo.id == req.params.id
-    })
-    console.log(singleTodo);
-    res.json(singleTodo)
+  console.log(req.params.id);
+  const singleTodo = todos.filter((todo) => {
+    return todo.id == req.params.id;
   });
-  
+  console.log(singleTodo);
+  res.json(singleTodo);
+});
 
-// *POST ENDPOINT
+// *POST ENDPOINT : Creating a todo
 router.post("/", (req, res) => {
   const todo = {
     id: 3,
@@ -30,14 +29,51 @@ router.post("/", (req, res) => {
   res.json({ msg: "todo created", todos: todos });
 });
 
+// *DELETE ENPOINT
+
+router.delete("/:id", (req, res) => {
+  const todoindex = todos.findIndex((todo) => todo.id == req.params.id);
+  todos.splice(todoindex, 1);
+  res.json({ msg: "todo deleted", todos: todos });
+});
+
+// *UPDATE ENDPOINT
+
+router.put("/:id", (req, res) => {
+  const todo = {
+    id: 3,
+    name: req.body.todo,
+    completed: true,
+  };
+  const todoindex = todos.findIndex((todo) => todo.id == req.params.id);
+  todos.splice(todoindex, 1, todo);
+  res.json({ msg: "todo updated", updatedTodo: todo });
+});
+
 module.exports = router;
 
 // TODO : install mangodb
 // TODO : configure it for machine
 // TODO : learn to use mangodb
-// TODO : learn connected mangodb to express app 
+// TODO : learn connected mangodb to express app
 
 // ? passportjs
 // ? mangose
 // ? JWT
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
